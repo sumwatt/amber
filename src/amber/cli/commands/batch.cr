fbf58da2aa08f691b89626c512a284f97af6ecd2
colson <colson@cogfactory.net>
2018-04-28 18:01:50 -0700

    basic prototype for Model/Controller

--- src/amber/cli/commands/batch.cr
+++ src/amber/cli/commands/batch.cr
@@ -6,31 +6,31 @@ module Amber::CLI
 
     class Batch < Command
       class Options
-        arg "file", desc: "filename.yml", required: true
+        arg "file", desc: "/path/to/filename.yml", required: true
         bool "--no-color", desc: "Disable colored output", default: false
         help
       end
 
       class Help
         header "Generate resources based on YAML file"
-        caption ""
+        caption "# Generate resources based on YAML file"
       end
 
       def run
         ensure_file_argument!
-        yaml = YAML.parse(File.read(args.file))
-        # Loads an array of YAML hashes
+        yaml = YAML.parse_all(File.read(args.file))
+        # Loads an YAML Array
         yaml.each do |request|
-          # get the action name "model/scaffold/contro"
           request.each do |action, data|
-            action_type = action.to_s
+            action_type = action.to_s.downcase
             templates = [] of Template
             data.each do |name, args|
               fields = [] of String
-              args.each {|arg| fields.push arg.to_s} if args
-              template = Template.new(name.to_s, ".", fields)
+              args.to_a.each {|arg| fields.push arg.to_s} if args
+              templates.push(Template.new(name.to_s.downcase, ".", fields))
             end
             templates.each do |template|
+              puts template
               template.generate action_type
             end
           end
@@ -47,7 +47,7 @@ module Amber::CLI
 
       private def ensure_file_argument!
         unless args.file?
-          CLI.logger.info "Parsing Error: The File argument is required.", "Error", :red
+          CLI.logger.info "File Error: A file path is required", "Error", :red
           exit! help: true, error: true
         end
       end
